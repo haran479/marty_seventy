0d428a604085c6b838ea43a5ed2ef0e9f1052084
haran479 <54596557+haran479@users.noreply.github.com>
2020-04-08 12:19:59 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+let marty; 
+
+
+function preload (){
+ marty = loadImage('marty.png');
+}
+
+
+function setup() {
+ createCanvas(500,6500);
+   
+}
+function draw (){ 
+  if (mouseIsPressed) {
+    
+  image (marty, mouseX, mouseY, 200, 200);
+}
+  else {
+    fill (mouseX+85, mouseY-100, mouseX-20);
+    noStroke ();
+    ellipse (mouseX, mouseY, 50,50);
+  }
+}
